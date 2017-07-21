2f5f09f43be91af3b3741fe59efef170435a3bbc
FRSCO <frscollo@hotmail.it>
2017-07-21 17:06:19 +0100

    initial commit

---
+++ django_todo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\frsco\hello_django\django_rest_framework\django_todo\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ django_todo/todo/admin.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+from django.contrib import admin
+from todo.models import Todo
+
+admin.site.register(Todo)
